e276847ff0ebca402cb383ea78073863688366e0
Ice0608 <icecreeper0608@gmail.com>
2026-06-23 14:39:05 +0800

    update yuran

--- resources/views/program/_guest_tab_yuran.blade.php
+++ resources/views/program/_guest_tab_yuran.blade.php
@@ -197,14 +197,14 @@
                 @endif
                 <div class="mt-6 grid grid-cols-[minmax(0,1fr)_minmax(7.5rem,auto)] items-start gap-4 border-t border-gray-200 pt-4 text-sm font-semibold text-gray-900">
                     <span>Jumlah Yuran Pengajian</span>
-                    {{-- <span class="text-right">RM {{ number_format($pengajianTotal, 2) }}</span> --}}
+                    <span class="text-right">RM {{ number_format($pengajianTotal, 2) }}</span>
                 </div>
             </div>
 
             <div class="rounded-3xl border border-gray-200 bg-gray-50 p-6">
                 <div class="mb-4 grid grid-cols-[minmax(0,1fr)_minmax(7.5rem,auto)] items-start gap-4">
                     <h3 class="font-semibold leading-snug text-gray-800">Elaun / Bantuan Sara Hidup</h3>
-                    <span class="text-right text-sm font-semibold leading-snug text-gray-900">RM {{ number_format($elaunTotal, 2) }}</span>
+                    {{-- <span class="text-right text-sm font-semibold leading-snug text-gray-900">RM {{ number_format($elaunTotal, 2) }}</span> --}}
                 </div>
                 @if($kursus->elauns->isNotEmpty())
                     <div class="space-y-4">
