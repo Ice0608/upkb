8af4f903728b1af849cab3d92cc749d36ebfdf2d
Ice0608 <icecreeper0608@gmail.com>
2026-06-26 10:28:41 +0800

    fix edit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -35,3 +35,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$publicPath = env('APP_PUBLIC_PATH');
+
+if (filled($publicPath)) {
+    $app->usePublicPath($publicPath);
+}
+
+return $app;
